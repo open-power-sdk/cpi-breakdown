f9010bd7f9a0241e558d18e9a292e656341b1ae5
Daniel Kreling <dbkreling@br.ibm.com>
2017-01-02 15:25:14 -0200

    tools-cpi: add --all to info feature
    Adding -a or --all to display information for all events and metrics supported by
    CPI (taken from their respective .yaml files)
    
    Change-Id: Ie0eb49d7be981cca1f2966c7e59cb1386aa36688
    Signed-off-by: Daniel Kreling <dbkreling@br.ibm.com>

--- cpi/info/info_handler.py
+++ cpi/info/info_handler.py
@@ -25,12 +25,10 @@ from cpi import metrics_calculator
 
 
 class Metric(object):
-    """ Create an metric object containing name, formula, components, and
-        description"""
-    def __init__(self, name, formula, components, description):
+    """ Class used to create metric objects """
+    def __init__(self, name, formula, description):
         self.name = name
         self.formula = formula
-        self.components = components
         self.description = description
 
     def get_name(self):
@@ -39,11 +37,13 @@ class Metric(object):
     def get_formula(self):
         return self.formula
 
-    def get_components(self):
-        return self.components
-
     def get_description(self):
         return self.description
+
+    def __eq__(self, name):
+        """ Object of this class is equal if the name received is equals
+        to this class name attribute """
+        return self.name == name
 
 
 class InfoHandler(object):
@@ -53,89 +53,83 @@ class InfoHandler(object):
     METRICS = metrics_calculator.MetricsCalculator(PROCESSOR)
 
     def __init__(self):
-        self.events_keys = []
-        self.metrics_keys = []
-        self.event_names = self.EVENTS_LIST.get_events()
-        self.mo_list = []
-        self.__load_metrics()
+        event_names = self.EVENTS_LIST.get_events()
+        self.events_list = self.__get_events_list(event_names)
+        self.metrics_list = self.__get_metrics_list()
 
-    def show_info(self, occurrence):
+    def show_info(self, occurrence_item, all_opt):
         """ Calls the proper function to display event or metric info """
-        # Load events names in a list (self.events_keys).
-        self.events_keys = self.__load_events()
-        # The same for metric objects (self.mo_list).
-        self.mo_list = self.__load_metrics()
+        if all_opt:
+            self.__show_all()
+            sys.exit(0)
 
-        if occurrence in self.events_keys:
+        occurrence = occurrence_item[0]
+        if occurrence in self.events_list:
             self.__print_events_info(occurrence)
-        elif occurrence in self.metrics_keys:
+        elif occurrence in self.metrics_list:
             self.__print_metrics_info(occurrence)
         else:
             print "Event or Metric \"{}\" not found.".format(occurrence)
             sys.exit(1)
         sys.exit(0)
 
-    def __load_events(self):
-        """ Return events keys (names) in a list"""
-        # Load events names in a list (self.events_keys).
-        return self.__events_keys_list()
-
-    def __load_metrics(self):
-        """ Return metric objects in a list (self.mo_list) """
-        return self.__get_metrics_list()
+    def __show_all(self):
+        """ Show information of all resources (metrics and events) """
+        for event in self.events_list:
+            self.__print_events_info(event)
+            print "\n"
+        for obj in self.metrics_list:
+            self.__print_metrics_info(obj.get_name())
+            print "\n"
+        return 0
 
     def __print_events_info(self, occurrence_event):
         """ Print information about events """
-        print "    Type:\n\tEvent"
-        print "    Event Name:\n\t", occurrence_event
-        print "    Description:\n\t", \
+        print "    Event Name:\t", occurrence_event
+        print "    Type:\tEvent"
+        print "    Description:", \
             self.EVENTS_LIST.get_event_description(occurrence_event)
         return 0
 
     def __print_metrics_info(self, occurrence_metric):
         """ Display information about metric_name if it is in the
            metrics_list."""
-        print "    Type:\n\tMetric"
-        print "    Metric Name:\n\t", self.get_metric_name(occurrence_metric)
-        print "    Formula:\n\t", self.get_metric_formula(occurrence_metric)
-        print "    Components:\n\t",\
-            self.get_metric_components(occurrence_metric)
-        print "    Description:\n\t",\
+        print "    Name:\t", self.get_metric_name(occurrence_metric)
+        print "    Type:\tMetric"
+        print "    Formula:\t", self.get_metric_formula(occurrence_metric)
+        print "    Description:",\
             self.get_metric_description(occurrence_metric)
         return 0
 
-    def __events_keys_list(self):
+    def __get_events_list(self, event_names):
         """ Create a list of all event_keys."""
-        for array in self.event_names:
+        events_list = []
+        for array in event_names:
             for event in array:
-                self.events_keys.append(event)
-        return self.events_keys
+                events_list.append(event)
+        return events_list
 
     def __get_metrics_list(self):
-        """ Return a list of objects (self.mo_list) with all metric
-            information (keys) """
+        """ Return a list of metrics objects """
+        metric_list = []
         # Populate the list
         for key in self.METRICS.get_raw_metrics().keys():
-            # set metrics instance variables:
             name = self.METRICS.get_raw_metrics()[key]["NAME"]
             formula = self.METRICS.get_raw_metrics()[key]["FORMULA"]
-            components = self.METRICS.get_raw_metrics()[key]["COMPONENTS"]
             description = self.METRICS.get_raw_metrics()[key]["DESCRIPTION"]
-            mo = Metric(name, formula, components, description)
-            self.metrics_keys.append(name)
-            # append mo instance to the instance list
-            self.mo_list.append(mo)
-        return self.mo_list
+            metric = Metric(name, formula, description)
+            metric_list.append(metric)
+        return metric_list
 
-    def __get_object_from_mo_list(self, occurrence_metric):
+    def __get_object_from_metric_list(self, occurrence_metric):
         """ Load the metric object variable values in obj """
-        obj = [x for x in self.mo_list if x.name == occurrence_metric]
+        obj = [x for x in self.metrics_list if x.name == occurrence_metric]
         return obj
 
     def get_metric_name(self, occurrence_metric):
         """Return metric name"""
         try:
-            metric = self.__get_object_from_mo_list(occurrence_metric)[0]
+            metric = self.__get_object_from_metric_list(occurrence_metric)[0]
             return metric.get_name()
         except IndexError:
             return None
@@ -143,7 +137,7 @@ class InfoHandler(object):
     def get_metric_formula(self, occurrence_metric):
         """Return metric formula """
         try:
-            metric = self.__get_object_from_mo_list(occurrence_metric)[0]
+            metric = self.__get_object_from_metric_list(occurrence_metric)[0]
             return metric.get_formula()
         except IndexError:
             return None
@@ -151,7 +145,7 @@ class InfoHandler(object):
     def get_metric_components(self, occurrence_metric):
         """ Return metric components """
         try:
-            metric = self.__get_object_from_mo_list(occurrence_metric)[0]
+            metric = self.__get_object_from_metric_list(occurrence_metric)[0]
             if metric.get_components():
                 return ", ".join(metric.get_components())
             else:
@@ -162,7 +156,7 @@ class InfoHandler(object):
     def get_metric_description(self, occurrence_metric):
         """ Return metric description """
         try:
-            metric = self.__get_object_from_mo_list(occurrence_metric)[0]
+            metric = self.__get_object_from_metric_list(occurrence_metric)[0]
             return metric.get_description()
         except IndexError:
             return None
