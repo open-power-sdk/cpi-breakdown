5e0cac5c392e144a15760d308157e4307a012a8c
Rafael Peria de Sene <rpsene@br.ibm.com>
2017-01-18 11:24:24 -0200

    tools-cpi: remove LICENSE from installation
    Change-Id: I80ca665c813597bdfa459febaaa6e34f7a9637b4
    Signed-off-by: Rafael Peria de Sene <rpsene@br.ibm.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2016 IBM Corporation
+Copyright (C) 2016, 2017 IBM Corporation
 
 Licensed under the Apache License, Version 2.0 (the “License”);
 you may not use this file except in compliance with the License.
@@ -18,6 +18,9 @@ limitations under the License.
 
     Contributors:
         * Rafael Sene <rpsene@br.ibm.com>
+        * Daniel Kreling <dbkreling@br.ibm.com>
+        * Roberto Oliveira <rdutra@br.ibm.com>
+        * Diego Fernandez-Merjildo <merjildo@br.ibm.com>
 """
 
 from setuptools import setup, find_packages
@@ -32,22 +35,23 @@ requirements = [str(required.req) for required in requirements_list]
 setup(
     name='cpi',
     version='1.0.timestamp',
-    description='Profiles C/C++ applications with the CPI (cycles per instruction) breakdown model for POWER8.',
+    description='Profiles C/C++ applications with the CPI (cycles per instruction) breakdown model for POWER8',
     long_description=readme,
-    author='Rafael Peria de Sene',
-    author_email='rpsene@br.ibm.com',
+    author='Rafael Peria de Sene, Roberto Guimarães Dutra de Oliveira, \
+Daniel Battaiola Kreling, Diego Fernandez Merjildo',
+    author_email='rpsene@br.ibm.com, rdutra@br.ibm.com, \
+dbkreling@br.ibm.com, merjildo@br.ibm.com',
     url='https://www-304.ibm.com/webapp/set2/sas/f/lopdiags/sdklop.html',
     license='Apache Software License 2.0',
     install_requires=requirements,
     packages=find_packages(exclude=("tests",)),
-    data_files=[("", ["LICENSE"])],
     include_package_data=True,
     test_suite='nose.collector',
     tests_require=['nose'],
     scripts=['bin/cpi'],
     zip_safe=False,
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Operating System :: POSIX :: Linux',
